40bfd84ae38ee1b5aaeb4e7954a0435259a44fc2
Dartegnian <root@dartegnian.com>
2021-07-07 02:25:42 +0800

    Write basics posts component, get JSON data in parent component
    Pass data as props, fix indentation

--- dart-frontend-assessment/src/pages/ExerciseOne.vue
+++ dart-frontend-assessment/src/pages/ExerciseOne.vue
@@ -1,20 +1,30 @@
 <template>
-  <header>
-    <h1>{{ heading }}</h1>
-    <h3>{{ subheading }}</h3>
-  </header>
+	<header>
+		<h1>{{ heading }}</h1>
+		<h3>{{ subheading }}</h3>
+	</header>
+	<section class="posts-area">
+		<Posts v-for="post in data" :key="post.post_id" v-bind:postData="post"/>
+	</section>
 </template>
 
 <script lang="tsx">
 import { defineComponent } from "vue";
 
+import data from "@/data/posts.json";
+import Posts from "@/components/ExerciseOne/Posts.vue";
+
 export default defineComponent({
-  data() {
-    return {
-      heading: "Hello, Developer!",
-      subheading: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    };
-  },
+	components: {
+		Posts,
+	},
+	data() {
+		return {
+			heading: "Hello, Developer!",
+			subheading: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+			data,
+		};
+	},
 });
 </script>
 
