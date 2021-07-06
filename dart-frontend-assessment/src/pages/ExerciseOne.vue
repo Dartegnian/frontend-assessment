388cf6cdd39c1d4f957ed4f43697d348d090187f
Dartegnian <root@dartegnian.com>
2021-07-07 06:52:26 +0800

    Add mobile and desktop styling, confirm to mockup, add alt attrib to img

--- dart-frontend-assessment/src/pages/ExerciseOne.vue
+++ dart-frontend-assessment/src/pages/ExerciseOne.vue
@@ -1,9 +1,9 @@
 <template>
-	<header class="hero-banner w-100 d-flex flex-column align-items-center justify-content-center">
+	<header class="hero-banner container-fluid d-flex align-items-center justify-content-center">
 		<h1 class="hero-banner__heading text-light">{{ heading }}</h1>
 		<h3 class="hero-banner__subheading text-light">{{ subheading }}</h3>
 	</header>
-	<section class="posts-area">
+	<section class="posts-area d-flex flex-wrap justify-content-between">
 		<Posts v-for="post in data" :key="post.post_id" v-bind:postData="post"/>
 	</section>
 </template>
@@ -35,6 +35,7 @@ export default defineComponent({
 
 .hero-banner {
 	position: relative;
+	flex-direction: column;
 	min-height: 650px;
 	background-image: url(http://via.placeholder.com/1920x650);
     background-position: center;
@@ -42,7 +43,7 @@ export default defineComponent({
 	background-size: cover;
 
 	@include media-breakpoint-down(md) {
-		min-height: 600px;
+		min-height: 480px;
 		background-image: url(http://via.placeholder.com/600x600);
 	}
 
@@ -54,12 +55,25 @@ export default defineComponent({
 		margin-bottom: 1rem;
 		font-weight: bold;
 		font-size: 5rem;
+
+		@include media-breakpoint-down(md) {
+			font-size: 3rem;
+			line-height: 4rem;
+			text-align: center;
+			word-break: break-word;
+		}
 	}
 
 	&__subheading {
 		font-size: 2.5rem;
 		font-weight: 400;
+		text-align: center;
 		letter-spacing: 1px;
+
+		@include media-breakpoint-down(md) {
+			font-size: 1.5rem;
+			line-height: 2.5rem;
+		}
 	}
 
 	&::after {
@@ -69,6 +83,16 @@ export default defineComponent({
 		height: 100%;
 		opacity: 0.3;
 		background-color: #000;
+	}
+}
+
+.posts-area {
+    margin: 0 auto;
+    margin-top: 3rem;
+	width: 75%;
+
+	@include media-breakpoint-down(md) {
+		width: 85%;
 	}
 }
 </style>
