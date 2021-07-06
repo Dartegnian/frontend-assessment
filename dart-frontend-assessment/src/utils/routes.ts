19bd5fd148dac944389ac7820866ac6feeaf0e50
Dartegnian <root@dartegnian.com>
2021-07-06 22:47:29 +0800

    Create routes for application using vue-router

---
+++ dart-frontend-assessment/src/utils/routes.ts
@@ -0,0 +1,17 @@
+import { Router, createRouter, createWebHashHistory } from "vue-router";
+import { Component } from "vue";
+
+import LandingPage from "@/pages/LandingPage.vue";
+import ExerciseOne from "@/pages/ExerciseOne.vue";
+
+const routes: Array<{ path: string, component: Component }> = [
+	{ path: "/", component: LandingPage },
+	{ path: "/exercise-one", component: ExerciseOne }
+]
+
+const router: Router = createRouter({
+	history: createWebHashHistory(),
+	routes
+});
+
+export default router;
