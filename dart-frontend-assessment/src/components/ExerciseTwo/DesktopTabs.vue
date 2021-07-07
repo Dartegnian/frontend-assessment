6ca6b057e69bb343508ff4c5d03c90ebbae1d5c3
Dartegnian <root@dartegnian.com>
2021-07-07 20:40:30 +0800

    Add proper classes, rename elements, break long lines

--- dart-frontend-assessment/src/components/ExerciseTwo/DesktopTabs.vue
+++ dart-frontend-assessment/src/components/ExerciseTwo/DesktopTabs.vue
@@ -1,14 +1,22 @@
 <template>
-	<section class="desktop-tabs">
-		<header
-			:key="section.title"
-			@click="changeSelection(index)"
-			v-for="(section, index) in sectionData"
-		>
-			{{ section.title }}
+	<article class="desktop-tabs mt-5">
+		<header class="desktop-tabs__header d-flex justify-content-between">
+			<button
+				:key="section.title"
+				@click="changeSelection(index)"
+				v-for="(section, index) in sectionData"
+				v-bind:class="'desktop-tabs__tab' +
+					(activeSelection === index ? ' desktop-tabs__tab--active' : '')
+				"
+			>
+				{{ section.title }}
+			</button>
 		</header>
-		<article v-html="sectionData[activeSelection].content"></article>
-	</section>
+		<section
+			class="desktop-tabs__content-area p-3 mt-4"
+			v-html="sectionData[activeSelection].content"
+		></section>
+	</article>
 </template>
 
 <script lang="ts">
