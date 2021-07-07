032324e34884a7368d184fa841a1ba01127d61e8
Dartegnian <root@dartegnian.com>
2021-07-07 19:55:45 +0800

    Desktop tabbed component with active tab content in main pane
    Write event emitters, props, elements

---
+++ dart-frontend-assessment/src/components/ExerciseTwo/DesktopTabs.vue
@@ -0,0 +1,32 @@
+<template>
+	<section class="desktop-tabs">
+		<header
+			:key="section.title"
+			@click="changeSelection(index)"
+			v-for="(section, index) in sectionData"
+		>
+			{{ section.title }}
+		</header>
+		<article v-html="sectionData[activeSelection].content"></article>
+	</section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	props: {
+		activeSelection: Number,
+		sectionData: Array,
+	},
+	methods: {
+		changeSelection(newSelection: Number) {
+			this.$emit("changeSelection", newSelection);
+		},
+	},
+	emits: ["changeSelection"],
+});
+</script>
+
+<style scoped lang="scss">
+</style>
