34927f95fd3146ca98d8c52f6629e0fa1a952a4c
Dartegnian <root@dartegnian.com>
2021-07-07 19:56:43 +0800

    Copy logic from desktop tabbed component, adjust for mobile accordion

---
+++ dart-frontend-assessment/src/components/ExerciseTwo/MobileAccordion.vue
@@ -0,0 +1,35 @@
+<template>
+	<section
+		:key="section.title"
+		@click="changeSelection(index)"
+		v-for="(section, index) in sectionData"
+		v-bind:class="
+			'desktop-tabs' + (activeSelection === index ? ' active' : '')
+		"
+	>
+		<header>
+			{{ section.title }}
+		</header>
+		<article v-html="section.content"></article>
+	</section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	props: {
+		activeSelection: Number,
+		sectionData: Array,
+	},
+	methods: {
+		changeSelection(newSelection: Number) {
+			this.$emit("changeSelection", newSelection);
+		},
+	},
+	emits: ["changeSelection"],
+});
+</script>
+
+<style scoped lang="scss">
+</style>
